a040dc2228ef45b18bf86deb5a8c0cb38af269aa
mashnoor3 <mashnoorrashid@gmail.com>
2018-01-23 21:52:05 -0500

    recursive solution for trees - top view

--- trees-top_view.py
+++ trees-top_view.py
@@ -1,8 +1,10 @@
-"""
-Node is defined as
-self.left (the left child of the node)
-self.right (the right child of the node)
-self.data (the value of the node)"""
+'''
+Question Info
+HackerRank
+Tree: Top View
+'''
+
+# Method 1: Solve iteratively. Use a queue and a hashtable
 
 import collections
 
@@ -16,18 +18,63 @@ def topView(root):
     # Hashtable values are Lists of the nodes, and key is horizontal distance (Hd)
     # away from the root. Root has Hd = 0
     hashTable = collections.defaultdict(list)
+    # Each element in the queue is a pair. [Node, Hd]
     queue = []
 
-    enqueue(queue, root)
+    enqueue(queue, [root, 0])
     hashTable[0].append(root)
 
     while len(queue) > 0:
-        currentNode = dequeue(queue)
+        current = dequeue(queue)
 
-        if currentNode.left:
-            enqueue(queue, currentNode.left)
-            # add it to the hashTable with corrent HD
+        if current[0].left:
+            # Enque the part: the left child node, and updated Hd
+            enqueue(queue, [current[0].left, current[1]-1 ])
+            # Using add the left child node to the hashtable, using the calculated Hd
+            hashTable[current[1]-1].append(current[0].left)
 
-        if currentNode.right:
-            enqueue(queue, currentNode.right)
-            # add it to the hashTable with corrent HD
+        if current[0].right:
+            # Enque the part: the right child node, and updated Hd
+            enqueue(queue, [current[0].right, current[1]+1])
+            # Using add the right child node to the hashtable, using the calculated Hd
+            hashTable[current[1]+1].append(current[0].right)
+
+    # Print the first item of the list for each entry in the Hashtable
+    for key in hashTable:
+        print (hashTable[key][0].data)
+
+
+
+# Method 2: Solve recursively. Traverse all the way left and print upwards to root
+# then traverse right and print along the way
+
+def goLeft(node):
+    # For left, need to traverse first then print
+    if node.left:
+        goLeft(node.left)
+    # Python 2
+    # print node.data,
+    # Python 3
+    print (node.data, end=" ")
+
+def goRight(node):
+    # For right, need to print as traversing down
+    # Python 2
+    # print node.data,
+    # Python 3
+    print (node.data, end=" ")
+
+    if node.right:
+        goRight(node.right)
+
+def topView(root):
+    if root.left:
+        goLeft(root.left)
+
+    # Python 2
+    # print root.data,
+    # Python 3
+    print (root.data, end=" ")
+
+    if root.right:
+        goRight(root.right)
