89fefbea687367a69121473ef21fb781caac00e7
mashnoor3 <mashnoorrashid@gmail.com>
2018-01-23 23:26:14 -0500

    binary search tree insertion

---
+++ trees-binary_search_tree_insertion.py
@@ -0,0 +1,28 @@
+'''
+Question Info
+HackerRank
+Tree: Binary Search Tree Insertion
+'''
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)"""
+
+
+def insert(root,val):
+    if root:
+        if val > root.data:
+            if root.right:
+                insert(root.right, val)
+            else:
+                root.right = Node(val)
+        else:
+            if root.left:
+                insert(root.left, val)
+            else:
+                root.left = Node(val)
+
+        return root
