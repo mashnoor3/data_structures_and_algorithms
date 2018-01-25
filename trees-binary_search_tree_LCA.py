b7cc8620b334f5edf59628fa79ec4e9ae5f378a4
mashnoor3 <mashnoorrashid@gmail.com>
2018-01-24 23:25:30 -0500

    BST LCA problem

---
+++ trees-binary_search_tree_LCA.py
@@ -0,0 +1,22 @@
+'''
+Question Info
+HackerRank
+Tree: Binary Search Tree Find LCA
+'''
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+
+def lca(root , v1 , v2):
+    if v1 > root.data and v2 > root.data:
+        parents.append(root)
+        lca(root.right, v1, v2)
+    elif v1 < root.data and v2 < root.data:
+        parents.append(root)
+        lca(root.left, v1, v2)
+    else:
+        return root
