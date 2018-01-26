230ff3f658608e605b768e763eb04efee8260e5d
mashnoor3 <mashnoorrashid@gmail.com>
2018-01-25 22:17:13 -0500

    LCA explanation

--- trees-binary_search_tree_LCA.py
+++ trees-binary_search_tree_LCA.py
@@ -12,9 +12,13 @@ self.data (the value of the node)
 """
 
 def lca(root , v1 , v2):
+    # Both are in the right subtree
     if v1 > root.data and v2 > root.data:
         lca(root.right, v1, v2)
+    # Both are in the left subtree
     elif v1 < root.data and v2 < root.data:
         lca(root.left, v1, v2)
     else:
+        # A node can be descendent of itself. So if v2 is a child of v1,
+        # then return v1
         return root
